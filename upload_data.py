af22b82e2a85d5ae2e35f0db1f2e7960e38d4980
Maximilian Golub <mgolub2@uw.edu>
2019-02-16 14:05:08 -0800

    Quick script for rsyncing data

--- upload_data.py
+++ upload_data.py
@@ -3,17 +3,27 @@ Uses drive linux/ anythgin to upload results to a specified dir
 """
 
 import argparse
+import os
 import subprocess
 
-CMD = '/usr/bin/rsync -r'
-
+CMD = '/usr/bin/rsync -p'
+TARGETS = ['l2_{}.npz', 'param_hist_{}.npz']
 
 p = argparse.ArgumentParser()
 p.add_argument('-d', '--dir', help='The upload directory')
 p.add_argument('data', nargs='+', help='The data directorys')
-p.add_argument('-i', '--iteration', help='The iteration to count to', default='1000')
+p.add_argument('-i', '--iteration', help='The iteration to count to', default=1000, type=int)
 p.add_argument('-u', '--upload_cmd', default=CMD)
 
 args = p.parse_args()
-cmd_to_run = args.upload_cmd.split(' ') + args.data + args.dir
-subprocess.run(cmd_to_run)
+
+transfers = []
+for d in args.data:
+    for i in range(0, args.iteration, 100):
+        for t in TARGETS:
+            src = os.path.join(d, t.format(i))
+            dest = args.dir + t.format(i)
+            transfers.append((src, dest))
+for t in transfers:
+    cmd_to_run = args.upload_cmd.split(' ') + t
+    subprocess.run(cmd_to_run)
