ce97940d001f3db450946c6635cbcd821a7eed04
Maximilian Golub <mgolub2@uw.edu>
2019-02-16 15:44:50 -0800

    ???

--- cifar10/train_cifar.py
+++ cifar10/train_cifar.py
@@ -123,8 +123,8 @@ def run_training(
     test_iter = iterators.SerialIterator(valid, batchsize, repeat=False, shuffle=False)
 
     #Init model
-    # x = net.xp.random.randn(1, 3, 32, 32).astype(np.float32)
-    # net(x)
+    x = net.xp.random.randn(1, 3, 32, 32).astype(np.float32)
+    net(x)
     # print("Ran network once")
     net = L.Classifier(net)
     # Optimizer
