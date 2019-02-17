36313477ae247437af43b89168ed78c88fd2ae73
Maximilian Golub <mgolub2@uw.edu>
2019-02-16 16:01:52 -0800

    ???

--- cifar10/train_cifar.py
+++ cifar10/train_cifar.py
@@ -145,8 +145,7 @@ def run_training(
     trainer.extend(extensions.LogReport())
     trainer.extend(extensions.observe_lr())
     if tracked == 0 and use_pruning:
-        masks = pruning.create_model_mask(net, use_pruning)
-        trainer.extend(pruning.pruned(net, masks))
+        trainer.extend(pruning.pruned(net))
     trainer.extend(extensions.Evaluator(
         test_iter, net, device=devices), name='val')
     trainer.extend(extensions.PrintReport(
