8e646f948e30f98c9b0e36d15fc1f824c6bc4cca
Maximilian Golub <mgolub2@uw.edu>
2019-02-19 23:52:55 -0800

    UGGG

--- cifar10/vgg.py
+++ cifar10/vgg.py
@@ -1,75 +1,56 @@
-"""
-Based on the chainer examples on github
-"""
-from __future__ import print_function
-
 import chainer
 import chainer.functions as F
 import chainer.links as L
 
-#~15M, real vgg-16 is 138M
 
 class Block(chainer.Chain):
 
     """A convolution, batch norm, ReLU block.
-
     A block in a feedforward network that performs a
     convolution followed by batch normalization followed
     by a ReLU activation.
-
     For the convolution operation, a square filter size is used.
-
     Args:
         out_channels (int): The number of output channels.
         ksize (int): The size of the filter is ksize x ksize.
         pad (int): The padding to use for the convolution.
-
     """
 
-    def __init__(self, out_channels, ksize, pad=1, xorshift=False, in_channels=None):
+    def __init__(self, out_channels, ksize, pad=1):
         super(Block, self).__init__()
-        w = chainer.initializers.HeNormal()
         with self.init_scope():
-            self.conv = L.Convolution2D(in_channels, out_channels, ksize, pad=pad,
-                                        nobias=True, initialW=w)
-            self.bn = L.BatchNormalization(out_channels, eps=1e-3)
+            self.conv = L.Convolution2D(None, out_channels, ksize, pad=pad,
+                                        nobias=True)
+            self.bn = L.BatchNormalization(out_channels)
 
-    def __call__(self, x):
+    def forward(self, x):
         h = self.conv(x)
         h = self.bn(h)
-        h = F.relu(h)
-        return h
+        return F.relu(h)
 
 
 class VGG(chainer.Chain):
 
     """A VGG-style network for very small images.
-
     This model is based on the VGG-style model from
     http://torch.ch/blog/2015/07/30/cifar.html
     which is based on the network architecture from the paper:
     https://arxiv.org/pdf/1409.1556v6.pdf
-
     This model is intended to be used with either RGB or greyscale input
     images that are of size 32x32 pixels, such as those in the CIFAR10
     and CIFAR100 datasets.
-
     On CIFAR10, it achieves approximately 89% accuracy on the test set with
     no data augmentation.
-
     On CIFAR100, it achieves approximately 63% accuracy on the test set with
     no data augmentation.
-
     Args:
         class_labels (int): The number of class labels.
-
     """
 
-    def __init__(self, class_labels=10, in_channels=None):
+    def __init__(self, class_labels=10):
         super(VGG, self).__init__()
-        w = chainer.initializers.HeNormal()
         with self.init_scope():
-            self.block1_1 = Block(64, 3, in_channels=in_channels)
+            self.block1_1 = Block(64, 3)
             self.block1_2 = Block(64, 3)
             self.block2_1 = Block(128, 3)
             self.block2_2 = Block(128, 3)
@@ -82,11 +63,11 @@ class VGG(chainer.Chain):
             self.block5_1 = Block(512, 3)
             self.block5_2 = Block(512, 3)
             self.block5_3 = Block(512, 3)
-            self.fc1 = L.Linear(None, 512, nobias=True, initialW=w)
-            self.bn_fc1 = L.BatchNormalization(512, eps=1e-3)
-            self.fc2 = L.Linear(None, class_labels, nobias=True, initialW=w)
+            self.fc1 = L.Linear(None, 512, nobias=True)
+            self.bn_fc1 = L.BatchNormalization(512)
+            self.fc2 = L.Linear(None, class_labels, nobias=True)
 
-    def __call__(self, x):
+    def forward(self, x):
         # 64 channel blocks:
         h = self.block1_1(x)
         h = F.dropout(h, ratio=0.3)
